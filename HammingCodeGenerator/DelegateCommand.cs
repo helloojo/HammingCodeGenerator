97f9c71e0b99eb062414aa357a44d20f655beb89
rueng564 <rueng564@gmail.com>
2019-10-11 13:06:16 +0900

    초기 커밋.

---
+++ HammingCodeGenerator/DelegateCommand.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HammingCodeGenerator
+{
+    class DelegateCommand : ICommand
+    {
+        private Action<object> _execute;
+
+        private Predicate<object> _canExecute;
+
+        private event EventHandler OnCanExecuteChangedInternal;
+
+        public DelegateCommand(Action execute)
+            : this(new Action<object>((object o) => execute()), DefaultCanExecute)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute)
+            : this(execute, DefaultCanExecute)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute ?? throw new ArgumentNullException("canExecute");
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+                OnCanExecuteChangedInternal += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+                OnCanExecuteChangedInternal -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute != null && _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            EventHandler handler = OnCanExecuteChangedInternal;
+            if (handler != null)
+            {
+                handler.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Destroy()
+        {
+            _canExecute = _ => false;
+            _execute = _ => { return; };
+        }
+
+        private static bool DefaultCanExecute(object parameter)
+        {
+            return true;
+        }
+    }
+}
